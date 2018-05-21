953f7a89748729d5bd8aa2bd276c7f2c084ddefd
Reyes Montemayor <sjpete1234@hotmail.com>
2018-05-21 12:45:54 +0900

    finish up assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "questions#ask"
 end
+
+  # Prefix Verb URI Pattern       Controller#Action
+  #    ask GET  /ask(.:format)    questions#ask
+  # answer GET  /answer(.:format) questions#answer
